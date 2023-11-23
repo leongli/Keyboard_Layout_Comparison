485312ee6f753f6417c140efe97566a882e9ff33
Leon <leong.li@hotmail.com>
2023-11-23 00:17:23 -0500

    progress on create

--- app/src/main/java/com/example/keyboardlayoutcomparison/QwertyTesting.java
+++ app/src/main/java/com/example/keyboardlayoutcomparison/QwertyTesting.java
@@ -2,8 +2,10 @@ package com.example.keyboardlayoutcomparison;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,13 +16,24 @@ import java.util.ArrayList;
 
 public class QwertyTesting extends Activity {
 
+    private final static String MYDEBUG = "MYDEBUG";
+    private final static int NUMBER_OF_QUESTIONS=5;
+
     TextView textToType, timeLabel;
     EditText userInput;
-    ArrayList<String>testPhaseList;
-    //testing set => 1=QWERTY, 2=DVORAK, 3=MESSAGEASE
-    int questionPointer, testingSet;
+    ArrayList<String>testPhaseList, qwertyPhraseList, dvorakPhraseList, messageasePhraseList;
+    int questionPointer,keyboard;
     UserInputListener userTextChangedListener;
     Button nextPhase;
+    float qwertyWPM, dvorakWPM, messageaseWPM, qwertyErrorRate, dvorakErrorRate, messageaseErrorRate;
+    private final static int QWERTY=0;
+    private final static int DVORAK=1;
+    private final static int MESSAGEASE=2;
+
+    int qwertyErrors, dvorakErrors, messageaseErrors;
+    long currentPhaseTime, qwertyStartTime, qwertyStopTime, dvorakStartTime, dvorakStopTime, messageaseStartTime, messageaseStopTime;
+    CountDownTimer countDownTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -36,10 +49,50 @@ public class QwertyTesting extends Activity {
         testPhaseList = generatePhraseSet();
         textToType.setText(testPhaseList.get(questionPointer));
     //set other environment variables
-        testingSet=1;
-        //initialize stats for each keyboard layout wpm, error rate
+        keyboard=QWERTY;
+    //initialize stats for each keyboard layout wpm, error rate
+        qwertyWPM=0f;
+        qwertyErrorRate=0f;
+        qwertyStopTime=0;
+        qwertyStartTime=0;
+        qwertyPhraseList=new ArrayList<>();
+        qwertyErrors=0;
+
+        dvorakWPM=0f;
+        dvorakErrorRate=0f;
+        dvorakStartTime=0;
+        dvorakStopTime=0;
+        dvorakPhraseList=new ArrayList<>();
+        dvorakErrors=0;
+
+        messageaseWPM=0f;
+        messageaseErrorRate=0f;
+        messageaseStartTime=0;
+        messageaseStopTime=0;
+        messageasePhraseList= new ArrayList<>();
+        messageaseErrors=0;
+
 
     //create timer
+        currentPhaseTime=0;
+        countDownTimer= new CountDownTimer(Long.MAX_VALUE, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                currentPhaseTime++;
+                long mills=currentPhaseTime;
+                int seconds= (int)(mills/60);
+                int minutes= (int) (seconds/60);
+                String time = String.format("%02d:%02d", seconds, mills);
+                timeLabel.setText(time);
+                Log.i("TIME", currentPhaseTime+" \t"+time);
+            }
+
+            @Override
+            public void onFinish() {
+                //
+            }
+        };
+
 
     //listener for each character typed, object class defined at very bottom of this activity
         userTextChangedListener = new UserInputListener();
